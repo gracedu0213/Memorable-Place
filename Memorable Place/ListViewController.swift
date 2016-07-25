697e91b81d2047fd50233dac2a47e9fa8c3dd6cf
Grace Du <gracedu0213@gmail.com>
2016-07-24 23:49:25 -0400

    First commit

--- Memorable Place/ListViewController.swift
+++ Memorable Place/ListViewController.swift
@@ -9,19 +9,34 @@
 import Foundation
 import UIKit
 
+var placeList = NSMutableArray()
+var returnIndex: Int = -1
+//let defaults = NSUserDefaults.standardUserDefaults()
+
+
 class ListViewController: UIViewController,UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
     
     var myTableView = UITableView()
-    var placeList: NSMutableArray? = []
+    
     
     override func viewDidLoad() {
         
+        self.title = "My favorite places"
         myTableView.dataSource = self
         myTableView.delegate = self
         myTableView.frame = CGRectMake(0, 20, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height)
         myTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         self.view.addSubview(myTableView)
         
+        self.navigationItem.hidesBackButton = true
+        let leftBarButton = UIBarButtonItem(title: "Map", style: .Bordered, target: self, action: #selector(map))
+        self.navigationItem.leftBarButtonItem = leftBarButton
+//        defaults.setObject(placeList[returnIndex], forKey: "myPlaceList")
+//        defaults.synchronize()
+    }
+    
+    func map(sender:UIBarButtonItem){
+        self.navigationController?.pushViewController(ViewController(), animated: true)
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -29,13 +44,38 @@ class ListViewController: UIViewController,UITextFieldDelegate, UITableViewDeleg
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return placeList!.count
+        return placeList.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
-        cell.textLabel?.text = placeList?[indexPath.row] as? String
+        let addedPlace:FavPlace = placeList.objectAtIndex(indexPath.row) as! FavPlace
+        cell.textLabel!.text = addedPlace.address
         return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        returnIndex = indexPath.row
+        print(returnIndex)
+        self.navigationController?.pushViewController(ViewController(), animated: true)
+    }
+    
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        returnIndex = indexPath.row
+        return indexPath
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        if editingStyle == .Delete{
+            placeList.removeObjectAtIndex(indexPath.row)
+            tableView.reloadData()
+        }
+    }
+
+        
+    override func viewWillAppear(animated: Bool) {
+        myTableView.reloadData()
     }
     
     override func didReceiveMemoryWarning() {
--- Memorable Place/ViewController.swift
+++ Memorable Place/ViewController.swift
@@ -10,85 +10,170 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate{
-  
-  var myMapView = MKMapView()
+class FavPlace{
+  var address:String
+  var lat:CLLocationDegrees = 0
+  var lon:CLLocationDegrees = 0
+  init (name:String){
+    self.address = name
+  }
+}
 
+class ViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate{
+  
+
+  var myMapView = MKMapView()
+  var locationManager = CLLocationManager()
+  
+  var newPlace: FavPlace?
+  var updateView:Int = 0
+  
   override func viewDidLoad() {
-    
-    
     
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
-//    self.navigationItem.title = "Map"
-//    let barButtonRight = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(addPlaces))
-//    self.navigationItem.setRightBarButtonItem(barButtonRight, animated: true)
+    self.locationManager.delegate = self
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    self.locationManager.requestWhenInUseAuthorization()
     
+    if returnIndex == -1{
+      //add first annonation on the map
+      let latitude: CLLocationDegrees = 43.659431
+      let longitude: CLLocationDegrees = -79.385088
+      let latDelta: CLLocationDegrees = 0.02
+      let longDelta: CLLocationDegrees = 0.02
+      let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+      let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+      let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+      myMapView.setRegion(region, animated: true)
+    }
+    else{
+      print("return index \(returnIndex)")
+      let addedPlace:FavPlace = placeList[returnIndex] as! FavPlace
+      addAnnotation(addedPlace, updated: true)
+    }
     
-    //create the navigation bar
-    let navBar = UINavigationBar(frame: CGRectMake(0, 20, UIScreen.mainScreen().bounds.width, 44))
-    self.view.addSubview(navBar)
-    let navItem = UINavigationItem(title: "Map")
+    self.navigationItem.title = "Map"
     let barButtonRight = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(addPlaces))
-    navItem.setRightBarButtonItem(barButtonRight, animated: true)
-    navBar.setItems([navItem], animated: true)
+    self.navigationItem.setRightBarButtonItem(barButtonRight, animated: true)
     
     //create the map view
     myMapView.frame = CGRectMake(0, 64, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height - 64)
     self.view.addSubview(myMapView)
-    //43.659431, -79.385088 at 761 Bay Street
-    let latitude: CLLocationDegrees = 43.659431
-    let longitude: CLLocationDegrees = -79.385088
-    let latDelta: CLLocationDegrees = 0.1
-    let longDelta: CLLocationDegrees = 0.1
-    let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
-    let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
-    let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
-    myMapView.setRegion(region, animated: true)
   
     let ulpgr = UILongPressGestureRecognizer(target: self, action: #selector(pressAction))
     ulpgr.minimumPressDuration = 1
     myMapView.addGestureRecognizer(ulpgr)
-  
+    
+    
     }
   
   //long pressed function
   func pressAction(gestureRecognizer: UILongPressGestureRecognizer){
+    if gestureRecognizer.state == .Began{
     let touchPoint = gestureRecognizer.locationInView(myMapView)
-    let location: CLLocationCoordinate2D = myMapView.convertPoint(touchPoint, toCoordinateFromView: myMapView)
+    let coordinate: CLLocationCoordinate2D = myMapView.convertPoint(touchPoint, toCoordinateFromView: myMapView)
+    let location: CLLocation = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+    var title:String = ""
+      
+    CLGeocoder().reverseGeocodeLocation(location, completionHandler: {(placemarks, error) -> Void in
+      
+      if error != nil {
+        print("there is an error")
+      }
+      if placemarks?.count > 0{
+        let pm = CLPlacemark(placemark: placemarks![0])
+        var thoroughfare:String = ""
+        var subthoroughfare:String = ""
+        if pm.thoroughfare != nil{
+          thoroughfare = pm.thoroughfare!
+        }
+        if pm.subThoroughfare != nil{
+          subthoroughfare = pm.subThoroughfare!
+        }
+        
+        title = "\(thoroughfare) \(subthoroughfare)"
+
+      }
+      else {
+        print("Problem with the data received from geocoder")
+      }
+      
+      if title != " "{
+        self.newPlace = FavPlace(name: title)
+        self.newPlace?.lat = coordinate.latitude
+        self.newPlace?.lon = coordinate.longitude
+        placeList.addObject(self.newPlace!)
+      }
+      let annotation = MKPointAnnotation()
+      annotation.coordinate = coordinate
+      annotation.title = self.newPlace!.address
+      self.myMapView.addAnnotation(annotation)
+      
+    })
     
     //add alert window before add the new place to the list
-    let myAlert = UIAlertController(title:"Alert", message: "Add new favoriate place", preferredStyle: .Alert)
+    if gestureRecognizer.state == .Began{
+      var myAlertView = UIAlertController()
+      myAlertView = UIAlertController(title:"Alert", message: "Add new favoriate place", preferredStyle: .Alert)
     let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-    myAlert.addAction(cancelAction)
-    let addAction = UIAlertAction(title: "Add", style: .Default, handler: nil)
-    myAlert.addAction(addAction)
-    if(self.navigationController == nil){
-      print("nil")
+    myAlertView.addAction(cancelAction)
+      let addAction = UIAlertAction(title: "Add", style: .Default, handler: {action in self.pressed()})
+    myAlertView.addAction(addAction)
+    self.presentViewController(myAlertView, animated: true, completion: nil)
     }
-    //self.navigationController?.pushViewController(myAlert, animated: true)
-    //self.presentViewController(myAlert, animated: true, completion: nil)
-    
-    
-    
-//    let myAlert = UIAlertView()
-//    myAlert.addButtonWithTitle("Add")
-//    myAlert.addButtonWithTitle("Cancel")
-//    myAlert.title = "Alert"
-//    myAlert.message = "Add new favorite place?"
-//    myAlert.show()
-    
+    }
   }
   
+  func pressed(){
+    let vc = ListViewController()
+    self.navigationController?.pushViewController(vc, animated: true)
+  }
   
   //add new faviorate place
   func addPlaces(sender:UIButton!){
     print("pressed")
-    let navController = UINavigationController(rootViewController: ListViewController())
-    self.presentViewController(navController, animated: true, completion: nil)
-//    let vc = ListViewController() as ListViewController
-//    self.navigationController?.pushViewController(vc, animated: true)
+//    let navController = UINavigationController(rootViewController: ListViewController())
+//    self.presentViewController(navController, animated: true, completion: nil)
+    let vc = ListViewController() as ListViewController
+    self.navigationController?.pushViewController(vc, animated: true)
+  }
+  
+  func addAnnotation(addedPlace: FavPlace, updated: Bool){
+    let latitude: CLLocationDegrees = addedPlace.lat
+    let longitude: CLLocationDegrees = addedPlace.lon
+    let latDelta: CLLocationDegrees = 0.02
+    let longDelta: CLLocationDegrees = 0.02
+    let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+    let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+    let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+    myMapView.setRegion(region, animated: true)
+    let annotation = MKPointAnnotation()
+    annotation.coordinate = location
+    annotation.title = addedPlace.address
+    self.myMapView.addAnnotation(annotation)
+    if updated{
+    myMapView.selectAnnotation(annotation, animated: true)
+    }
+  }
+  
+  func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    let userLocation:CLLocation = locations[0]
+    let latitude: CLLocationDegrees = userLocation.coordinate.latitude
+    let longitude: CLLocationDegrees = userLocation.coordinate.longitude
+    let latDelta: CLLocationDegrees = 0.02
+    let longDelta: CLLocationDegrees = 0.02
+    let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+    let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+    let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+    self.myMapView.setRegion(region, animated: true)
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    for place in placeList{
+      addAnnotation(place as! FavPlace, updated: false)
+    }
   }
 
     override func didReceiveMemoryWarning() {
